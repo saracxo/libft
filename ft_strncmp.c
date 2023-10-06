a0f2e2722a9fcdc3e73370ffcf357a2802b6d25c
Sara Castilla Ortiz <sarcasti@c2r7s3.42madrid.com>
2023-10-06 16:38:19 +0200

    06102023dos

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,14 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sarcasti <sarcasti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sarcasti <sarcasti@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 16:25:28 by sarcasti          #+#    #+#             */
-/*   Updated: 2023/10/04 17:19:36 by sarcasti         ###   ########.fr       */
+/*   Updated: 2023/10/06 16:23:00 by sarcasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int strncmp(const char *str1, const char *str2, size_t n)
+#include "libft.h"
+
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
-    
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (*str1 - *str2 != 0)
+			return ((unsigned char) *str1 - (unsigned char) *str2); // uns char es para convertir los valores en positivo siempre. La resta es la diferencia en valores ASCII. 
+		str1++;
+		str2++;
+		i++;
+	}
+	return (0);
+}
+
+#include <stdio.h>
+int main(void)
+{
+	char str1[] = "uno";
+	char str2[] = "dos";
+
+	printf();
+	return (0);
 }
